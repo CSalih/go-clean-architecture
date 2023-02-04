a3b7a9be352174f0bfd1f9fee4b62b215da6a020
Salih Candir <salih@live.at>
2023-02-04 21:51:56 +0100

    feat: use lowercase names for json

--- internal/users/domain/model/user.go
+++ internal/users/domain/model/user.go
@@ -1,7 +1,7 @@
 package model
 
 type User struct {
-	Id       int
-	Username string
-	Status   string
+	Id       int    `json:"id"`
+	Username string `json:"username"`
+	Status   string `json:"status"`
 }
